a490d64b8b9b5c105a77f463e1076eb659a4d5a8
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2020-07-08 09:58:34 -0300

    Propriedades do componente

---
+++ 09-avancando-componentes/components/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <tasks-component :title="'Listagem de tarefas'" :items="['lavar', 'passar', 'cozinhar']"></tasks-component>
+    <list-items-component :title="'Listagem'" :items="['lavar', 'passar', 'cozinhar']"></list-items-component>
+  </div>
+</template>
+
+<script>
+import TasksComponent from "./components/TasksComponent.vue";
+import ListItemsComponent from "./components/ListItemsComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    TasksComponent,
+    ListItemsComponent
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 09-avancando-componentes/components/src/components/TasksComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title", "items"]
+};
+</script>
+
+<style>
+</style>
