238ca05071aa1e57a1b37b595c6c0adae7e7a3d4
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2020-07-09 07:29:42 -0300

    Vue resource busca de cep

---
+++ 12-vue-http/vue-resource-http/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <search-cep-component></search-cep-component>
+  </div>
+</template>
+
+<script>
+import SearchCepComponent from "./components/SearchCepComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    SearchCepComponent
+  }
+};
+</script>
+
+<style>
+</style>
