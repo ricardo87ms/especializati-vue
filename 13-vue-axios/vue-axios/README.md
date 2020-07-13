c6a9d719d67e7bbc343665d2034f37a8e5a989be
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2020-07-13 18:29:11 -0300

    Módulo de axios

---
+++ 13-vue-axios/vue-axios/README.md
@@ -0,0 +1,44 @@
+# vue-resource-http
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Run your tests
+
+```
+npm run test
+```
+
+### Lints and fixes files
+
+```
+npm run lint
+```
+
+### Comandos Utilizados
+
+```
+vue create vue-axios
+
+npm install axios
+
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ 13-vue-axios/vue-axios/src/components/SearchCepComponent.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <h1 v-text="title"></h1>
+    <form @submit.prevent="submit">
+      <input type="text" v-model="cep" />
+      <button type="submit">Buscar</button>
+    </form>
+    <div v-if="error">
+      <p>{{ error }}</p>
+    </div>
+    <div v-if="loading">
+      <img src="../assets/preloader.gif" alt />
+    </div>
+    <div v-show="address.bairro">
+      <p>Cidade: {{ address.localidade }}</p>
+      <p>Bairro: {{ address.bairro }}</p>
+      <p>Logradouro: {{ address.logradouro }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      title: "Busca por CEP",
+      cep: "",
+      address: {
+        bairro: ""
+      },
+      loading: false,
+      error: ""
+    };
+  },
+  methods: {
+    submit() {
+      this.clear();
+      axios
+        .get(`https://viacep.com.br/ws/${this.cep}/json/`)
+        .then(
+          response => {
+            if (response.data.erro) {
+              this.error = "Este CEP não existe";
+            } else {
+              this.address = response.data;
+            }
+          },
+          error => {
+            this.error = "Erro ao consultar o cep";
+            console.log(error);
+          }
+        )
+        .finally(() => {
+          this.loading = false;
+          this.cep = "";
+        });
+    },
+    clear() {
+      this.error = "";
+      this.address = {};
+      this.loading = true;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
