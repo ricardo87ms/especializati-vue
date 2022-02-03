0bf5a3fc00827ad7dfba6d949c46b6179e019e2a
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2022-02-02 21:08:10 -0300

    Realizando a validação do formulário no submit

--- 08-formularios/formulario-vue3/src/components/FormComponent.vue
+++ 08-formularios/formulario-vue3/src/components/FormComponent.vue
@@ -4,55 +4,70 @@
    <h1>{{ titulo }}</h1>
 
     <hr>
-    <input type="text" placeholder="Nome" v-model="userData.name" @blur="v$.userData.name.$touch">
-    <p v-if="v$.userData.name.$error">Name field has an error.</p>
-    {{ userData.name }}
 
-    <hr>
-    <div :class="{ 'is-danger' : v$.userData.email.$error } ">
-      <input type="email" placeholder="E-mail" v-model="userData.email" @blur="v$.userData.email.$touch">
-      <div v-if="v$.userData.email.$error">
-        <p v-for="(error, index) in v$.userData.email.$errors" :key="index">{{ error.$message }}</p>
+    <form @submit.prevent="saveData">
+
+      <div :class="{ 'is-danger' : v$.userData.name.$error }">
+        <input type="text" placeholder="Nome" v-model="userData.name" @blur="v$.userData.name.$touch">
+        <div v-if="v$.userData.name.$error">
+          <p v-for="(error, index) in v$.userData.name.$errors" :key="index">{{ error.$message }}</p>
+        </div>
+        {{ userData.name }}
       </div>
-      {{ userData.email }}
-    </div>
+      <hr>
 
-    <hr>
-    <input type="number" placeholder="Idade" v-model="userData.age">
-    {{ userData.age }}
+      <div :class="{ 'is-danger' : v$.userData.email.$error }">
+        <input type="email" placeholder="E-mail" v-model="userData.email" @blur="v$.userData.email.$touch">
+        <div v-if="v$.userData.email.$error">
+          <p v-for="(error, index) in v$.userData.email.$errors" :key="index">{{ error.$message }}</p>
+        </div>
+        {{ userData.email }}
+      </div>
 
-    <hr>
-    <input type="radio" name="sex" value="M" v-model="userData.sex"> Masculino  |
-    <input type="radio" name="sex" value="F" v-model="userData.sex"> Feminino
-    <p v-if="userData.sex">Sexo selecionado: {{ userData.sex }}</p>
+      <hr>
 
-    <hr>
-    <select v-model="userData.state">
-      <option value="">Selecione o Estado</option>
-      <option value="MG">Minas Gerais</option>
-      <option value="SP">São Paulo</option>
-      <option value="RJ">Rio de Janeiro</option>
-    </select>
-    <p v-if="userData.state">Estado selecionado: {{ userData.state }}</p>
+      <input type="number" placeholder="Idade" v-model="userData.age">
+      {{ userData.age }}
 
-    <hr>
-    <label for="agree">Concordo com os termos</label>
-    <input type="checkbox" id="agree" v-model="terms">
-    {{ terms }}
+      <hr>
+      <input type="radio" name="sex" value="M" v-model="userData.sex"> Masculino  |
+      <input type="radio" name="sex" value="F" v-model="userData.sex"> Feminino
+      <p v-if="userData.sex">Sexo selecionado: {{ userData.sex }}</p>
 
-    <hr>
-    <textarea cols="30" rows="10" v-model="description"></textarea>
-    <!-- <div style="white-space: pre;" v-html="description"></div> -->
-    <pre v-html="description"></pre>
+      <hr>
 
-    <hr>
-    <button type="submit">Enviar Agora</button>
+      <select v-model="userData.state">
+        <option value="">Selecione o Estado</option>
+        <option value="MG">Minas Gerais</option>
+        <option value="SP">São Paulo</option>
+        <option value="RJ">Rio de Janeiro</option>
+      </select>
+      <p v-if="userData.state">Estado selecionado: {{ userData.state }}</p>
+
+      <hr>
+
+      <label for="agree">Concordo com os termos</label>
+      <input type="checkbox" id="agree" v-model="terms">
+      {{ terms }}
+
+      <hr>
+
+      <textarea cols="30" rows="10" v-model="description"></textarea>
+      <!-- <div style="white-space: pre;" v-html="description"></div> -->
+      <pre v-html="description"></pre>
+
+      <hr>
+
+      <button type="submit">Enviar Agora</button>
+
+    </form>
+    <p v-if="submetido">{{ userData }}</p>
   </div>
 </template>
 
 <script>
 import useVuelidate from '@vuelidate/core'
-import { required, email } from '@vuelidate/validators'
+import { required, email, minLength, maxLength } from '@vuelidate/validators'
 
 export default {
   name: 'FormComponent',
@@ -69,15 +84,31 @@ export default {
         state: ''
       },
       terms: true,
-      description: ''
+      description: '',
+      submetido: false
     }
   },
   validations () {
     return {
       userData: {
-        name: { required },
+        name: { required, minLengthValue:
+        minLength(3),
+        maxLengthValue: maxLength(10) },
         email: { required, email }
       }
+    }
+  },
+  methods: {
+    async saveData() {
+      console.log('submetido!!!!');
+
+      const result = await this.v$.$validate()
+      if (!result) {
+        // notify user form is invalid
+        return
+      }
+
+      this.submetido = true;
     }
   }
 }
